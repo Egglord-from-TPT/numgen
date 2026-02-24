006daf495113967ed8e84c9a0bfbe28d8725e67c
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-02-24 13:53:31 +0200

    Rename package and update version and description

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="variables_plus",
-    version="2.3.0",
+    name="gen",
+    version="1.0.0",
     packages=find_packages(),
-    description="A simple library to create, clear, and delete global variables",
+    description="A simple library that generates a number.",
     author="",
     python_requires=">=3.7",
 )
